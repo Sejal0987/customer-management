6b1919e9bf4ec83196d09c21c0a94e1aa534dc07
Sejal <sejaljaiswal1430@gmail.com>
2022-09-09 23:28:20 +0530

    login-page

---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {SocialAuthService} from 'angularx-social-login';
+import {Router} from '@angular/router';
+import { Inject } from '@angular/core';
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+
+  constructor(@Inject(Router) private router: Router,
+  @Inject(SocialAuthService) public socialAuthServive: SocialAuthService) {
+  }
+
+  logout(): void {
+    this.socialAuthServive.signOut().then(() => this.router.navigate(['login']));
+  }
+
+}
